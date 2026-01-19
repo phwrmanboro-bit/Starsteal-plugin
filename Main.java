dcf8c3ee4abda8f2f2512a208d8c1f273f44fcd8
phwrmanboro-bit <phwrmanboro@gmail.com>
2026-01-19 20:32:36 +0530

    Initialize StarSteal plugin with enable/disable methods

--- Main.java
+++ Main.java
@@ -1,1 +1,24 @@
+package com.starsteal;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private static Main instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        getCommand("starsteal").setExecutor(new CommandManager());
+        getLogger().info("StarSteal Plugin Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("StarSteal Plugin Disabled!");
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+}
