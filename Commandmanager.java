e23c06182415a06b2671ca5b6423a38c82d3e0b3
phwrmanboro-bit <phwrmanboro@gmail.com>
2026-01-19 20:33:28 +0530

    Implement command handling for 'give' and 'recipe'

--- Commandmanager.java
+++ Commandmanager.java
@@ -1,1 +1,30 @@
+package com.starsteal;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandManager implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(args.length == 0) return false;
+
+        if(args[0].equalsIgnoreCase("give")) {
+            if(sender.hasPermission("starsteal.give") && args.length == 3){
+                Player target = sender.getServer().getPlayer(args[1]);
+                String star = args[2].toLowerCase();
+                CraftingManager.giveStar(target, star);
+            }
+        } else if(args[0].equalsIgnoreCase("recipe")) {
+            if(args.length == 2){
+                String star = args[1].toLowerCase();
+                CraftingManager.showRecipe((Player) sender, star);
+            }
+        }
+
+        return true;
+    }
+}
