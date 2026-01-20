6c9a641477658eafa20098057b44814d0a37ae73
phwrmanboro-bit <phwrmanboro@gmail.com>
2026-01-20 07:57:16 +0530

    Refactor Main.java to implement StarSteal plugin

--- Main.java
+++ Main.java
@@ -1,1 +1,39 @@
-getServer().getPluginManager().registerEvents(new AbilityManager(), this);
+package com.starsteal;
+
+import com.starsteal.ability.AbilityManager;
+import com.starsteal.cooldown.CooldownManager;
+import com.starsteal.star.StarManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StarSteal extends JavaPlugin {
+
+    private static StarSteal instance;
+    private CooldownManager cooldownManager;
+    private StarManager starManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        cooldownManager = new CooldownManager();
+        starManager = new StarManager(this);
+
+        getServer().getPluginManager().registerEvents(
+                new AbilityManager(this, cooldownManager, starManager),
+                this
+        );
+
+        getCommand("starsteal").setExecutor(new StarStealCommand(starManager));
+
+        getLogger().info("StarSteal plugin ENABLED");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("StarSteal plugin DISABLED");
+    }
+
+    public static StarSteal getInstance() {
+        return instance;
+    }
+}
